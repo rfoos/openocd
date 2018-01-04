83c5197b9ca94bf104dc63cc2a576ced5a05d0e0
Rick Foos <rick.foos@solengtech.com>
2018-01-04 09:00:21 -0600

    install: Add Ubuntu build packages, and windows path.
    (cherry picked from commit 090457c46d93d84019f3e57878888722321e9f77)

---
+++ ocd-winpath.bat
@@ -0,0 +1,5 @@
+:: Set user path after OpenOCD installation.
+:: Rick Foos <rfoos@solengtech.com>
+:: Add /M option to modify system path.
+
+setx PATH "%PATH%;C:\\Program Files (x86)\\GNU MCU Eclipse\\OpenOCD\\0.10.0-5-20171220-0432\\bin"
