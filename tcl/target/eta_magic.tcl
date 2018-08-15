34c3b1fcd5cda422e492b50dd9f4e10cd37a80ae
Rick Foos <rfoos@solengtech.com>
2018-08-14 20:59:47 -0500

    etacorem3/tcl: Remove smdw command.
    Change-Id: I62e1d1c568b82725059406ec8af9bc40ec445c80
    (cherry picked from commit 3e1ab4f0b130edd86c663910cc248633151ad8cb)

--- tcl/target/eta_magic.tcl
+++ tcl/target/eta_magic.tcl
@@ -7,6 +7,18 @@
 # Use setmagic command prior to running SRAM program.
 # Use clearmagic to run standard bootrom programs after reset.
 
+# This puts SWD in a slow mode to read/write without WAIT.
+proc swdslow { } {
+    ecm35xx.dap memaccess 400
+    adapter_khz 1000
+    echo "SWD in slow mode."
+}
+# This puts SWD in normal mode.
+proc swdnormal { } {
+    ecm35xx.dap memaccess 8
+    echo "SWD in normal mode."
+}
+
 proc setmagic  { } {
     mww 0x1001FFF0 0xc001c0de
     mww 0x1001FFF4 0xc001c0de
