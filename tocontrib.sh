f2656e63fb5b4c2c4484c057731f3d5090586c81
Rick Foos <rfoos@solengtech.com>
2018-08-29 09:25:12 -0500

    make: Update for load/store.
    Change-Id: Idbe6294f208d5435d9ecf87e437eeed1ace47d51

--- tocontrib.sh
+++ tocontrib.sh
@@ -8,6 +8,8 @@ OCDCONTRIB=~/openocd/contrib/loaders/flash/etacorem3
 cp ${OCDFIRMWARE}/read/src/read.c ${OCDCONTRIB}/.
 cp ${OCDFIRMWARE}/erase/src/erase.c ${OCDCONTRIB}/.
 cp ${OCDFIRMWARE}/write/src/write.c ${OCDCONTRIB}/.
+cp ${OCDFIRMWARE}/load/src/load.c ${OCDCONTRIB}/.
+cp ${OCDFIRMWARE}/store/src/store.c ${OCDCONTRIB}/.
 
 # include
 cp ${OCDFIRMWARE}/include/etacorem3_flash_common.h ${OCDCONTRIB}/.
--- tofirmware.sh
+++ tofirmware.sh
@@ -8,6 +8,8 @@ OCDCONTRIB=~/openocd/contrib/loaders/flash/etacorem3
 cp ${OCDCONTRIB}/read.c ${OCDFIRMWARE}/read/src/read.c 
 cp ${OCDCONTRIB}/erase.c ${OCDFIRMWARE}/erase/src/erase.c
 cp ${OCDCONTRIB}/write.c ${OCDFIRMWARE}/write/src/write.c
+cp ${OCDCONTRIB}/load.c ${OCDFIRMWARE}/load/src/load.c
+cp ${OCDCONTRIB}/store.c ${OCDFIRMWARE}/store/src/store.c
 
 # include
 cp ${OCDCONTRIB}/etacorem3_flash_common.h ${OCDFIRMWARE}/include/.
